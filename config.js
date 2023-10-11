48a25bd871e246c174ed1124891fe701a0e6d4e3
byhim27 <95236075+byhim27@users.noreply.github.com>
2023-10-11 21:29:43 +0700

    Him

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee loo Vợ 😍',
+    introDesc: `Anh có điều này muốn hỏi em, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Đồng Ý',
+    title: 'Em có yêu anh khoong😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em có yêu đấy nha  :> ',
+    btnYes: 'Em có , Yêu Anh  😊',
+    btnNo: 'Không nhaa !   ',
+    question:'Nói cho anh lý do vì sao em Yêu Anh điii <3',
+    btnReply: 'Gửi cho anh 😘',
+    reply: 'Tại vì anh là chồng emm  <3',
+    mess: 'Anh biết mà 🥰. Anh cũng yêu emm  😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không nhắn tin cho anh đi nào.',
+    btnAccept: 'Dạaaaaa ! <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100025247501919'
+}
